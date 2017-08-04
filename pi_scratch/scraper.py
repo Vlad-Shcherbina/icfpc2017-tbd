945ff3dffe0394a1ecd3eee5ed279726782ca458
Viaceslav Pozdniakov <slavap@wix.com>
2017-08-04 19:54:15 +0300

    Scraper setup

--- pi_scratch/scraper.py
+++ pi_scratch/scraper.py
@@ -3,15 +3,17 @@ import re
 import sys
 import urllib.request
 
+
 class Game(object):
-    def __init__(self, punters_no, punters_max, port, map_name):
-        self.punter_no = punters_no
+    def __init__(self, punters_num, punters_max, port, map_name):
+        self.punters_num = punters_num
         self.punters_max = punters_max
         self.port = port
         self.map_name = map_name
 
     def __repr__(self):
-        return f"Port={self.port}, punters_max={self.punters_max}, punter_no={self.punter_no}, map_name={self.map_name}."
+        return f"Port={self.port}, punters_max={self.punters_max}, punters_num={self.punters_num}, map_name={self.map_name}."
+
 
 def __parse_status(status):
     if 'Game in progress' in status or 'Offline' in status:
@@ -21,6 +23,7 @@ def __parse_status(status):
         return (int(l[1]), int(l[2]))
     else:
         sys.error('Unknown status: ' + status)
+
 
 def games():
     with urllib.request.urlopen('http://punter.inf.ed.ac.uk/status.html') as response:
@@ -32,6 +35,7 @@ def games():
             status = __parse_status(tds[0].string)
             if status:
                 yield Game(status[0], status[1], int(tds[2].string), tds[3].string)
+
 
 def main():
     for game in games():
