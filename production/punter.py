fbbe4a699cffe5db6387bb815152aac87f382bd8
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-08-05 06:42:07 +0200

    Fix broken tests (stuff not protected by if name main)

--- production/punter.py
+++ production/punter.py
@@ -4,5 +4,6 @@ from production.utils import config_logging
 from production.comms import offline_mainloop
 from production.dumb_bots import FirstMoveBot as Bot
 
-config_logging()
-offline_mainloop('tbd punter', Bot())
+if __name__ == '__main__':
+    config_logging()
+    offline_mainloop('tbd punter', Bot())
