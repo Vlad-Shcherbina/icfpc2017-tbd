c8be692218062c9e16461381a0ba37f2cdbe567e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-08-05 05:05:07 +0200

    Use comms in the online client

--- vlad_scratch/online_client.py
+++ vlad_scratch/online_client.py
@@ -16,66 +16,7 @@ from production.bot_interface import *
 from production import scraper
 from production import visualization
 from production import utils
-
-
-class CommsException(Exception):
-    pass
-
-class Connection:
-    def __init__(self, server, port, name):
-        self.name = name
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((server, port))
-        self.buf = bytearray()
-
-
-    def send(self, req):
-        s = json.dumps(req).encode()
-        s = b'%d:%b' % (len(s), s)
-        log.debug(f'{self.name} sending {s}')
-        self.socket.sendall(s)
-
-
-    def recv(self):
-        buf = self.buf
-        def recvsome():
-            data = self.socket.recv(4096)
-            if not data:
-                raise CommsException('Server unexpectedly closed connection')
-            buf.extend(data)
-
-        while True:
-            colon_pos = buf.find(b':')
-            if colon_pos >= 0:
-                break
-            recvsome()
-
-        msg_len = int(buf[:colon_pos].decode())
-        # basic sanity check, 10Mb should be enough for everyone
-        assert 0 <= msg_len <= 10_000_000
-
-        msg_end = colon_pos + 1 + msg_len
-        while len(buf) < msg_end:
-            recvsome()
-
-        res = json.loads(buf[colon_pos + 1 : msg_end])
-        del buf[:msg_end]
-        log.debug(f'{self.name} got {res}')
-        return res
-
-
-    def perform_handshake(self):
-        self.send({'me': self.name})
-        res = self.recv()
-        assert res['you'] == self.name
-
-
-    def get_request(self):
-        return self.recv()
-
-
-    def send_move(self, move):
-        self.send(move)
+from production import comms
 
 
 def main():
@@ -88,61 +29,40 @@ def main():
     logging.basicConfig(
         level=logging.INFO,
         format='%(levelname).1s %(module)10.10s:%(lineno)-4d %(message)s')
-    #log.setLevel(logging.DEBUG)
 
-    game = scraper.wait_for_game(predicate=lambda g: g.extensions==['futures'])
-
+    game = scraper.wait_for_game(
+        predicate=scraper.only_easy_eagers_p, extensions={'futures'})
     bot = dumb_bots.FirstMoveBot()
-
-    con = Connection('punter.inf.ed.ac.uk', game.port, 'tbd tbd')
-    print('conn')
-    con.perform_handshake()
 
     turn_number = 0
 
-    old_state = None
-    while True:
-        req = con.get_request()
-        if old_state is not None:
-            assert 'state' not in req
-            req['state'] = old_state
+    def cb(msg):
+        nonlocal turn_number
 
-        log.info(req)
-        req = json_format.parse_any_request(req)
+        if isinstance(msg, GameplayResponse):
+            log.info(f'my move: {msg.move}')
 
-        if isinstance(req, SetupRequest):
-            resp = bot.setup(req)
-            resp = json_format.format_setup_response(resp)
-        elif isinstance(req, GameplayRequest):
-            resp = bot.gameplay(req)
-            resp = json_format.format_gameplay_response(resp)
-        elif isinstance(req, ScoreRequest):
-            bot.score(req)
-            log.info(f'scores = {req.score_by_punter}')
-            break
-        else:
-            assert False
+        if isinstance(msg, (GameplayRequest, ScoreRequest)):
+            log.info(f'state.all_past_moves: {msg.state["all_past_moves"]}')
+            vis = visualization.Visualization(600, 600)
+            vis.draw_background()
+            map_ = json_format.parse_map(msg.state['map'])
+            vis.draw_map(map_)
+            vis.draw_legend(msg.state['punters'])
 
-        old_state = resp.pop('state')
-        #log.info(f'state = {old_state}')
+            for move in msg.state['all_past_moves']:
+                move = json_format.parse_move(move)
+                vis.draw_move(move, map_)
 
-        con.send_move(resp)
+            im = vis.get_image()
+            im.save(utils.project_root() / 'outputs' / f'{turn_number:04d}.png')
 
-        vis = visualization.Visualization(600, 600)
-        vis.draw_background()
-        map_ = json_format.parse_map(old_state['map'])
-        vis.draw_map(map_)
+            turn_number += 1
 
-        vis.draw_legend(old_state['punters'])
+        return msg
 
-        for move in old_state['all_past_moves']:
-            move = json_format.parse_move(move)
-            vis.draw_move(move, map_)
-
-        im = vis.get_image()
-        im.save(utils.project_root() / 'outputs' / f'{turn_number:04d}.png')
-
-        turn_number += 1
+    comms.online_mainloop(
+        'punter.inf.ed.ac.uk', game.port, 'tbdbd', bot, on_comms_cb=cb)
 
 
 if __name__ == '__main__':
