6a8a7d33bc219eacbc958fe0fbe88f997fc93172
Viaceslav Pozdniakov <slavap@wix.com>
2017-08-05 11:35:55 +0300

    Python rebuild script

---
+++ rebuild_tbd_python.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Uninstall all
+~/tbd_python/bin/pip3.6 uninstall -y -r requirements.txt
+
+# Install
+~/tbd_python/bin/pip3.6 install -r requirements.txt
+
+# Cleanup
+find ~/tbd_python/ -name '__pycache__' | xargs rm -rf
+find ~/tbd_python/ -name '*.a' | xargs rm
+rm -rf ~/tbd_python/lib/python3.6/test
+
+# Pack
+tar -cvJf tbd_python.tar.xz ~/tbd_python
+
