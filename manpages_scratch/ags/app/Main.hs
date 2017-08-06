3f1f359e0fb31cbc121f0cc4db0866ac0b260895
Viaceslav Pozdniakov <slavap@wix.com>
2017-08-07 00:13:38 +0300

    Scoreboard

--- manpages_scratch/ags/app/Main.hs
+++ manpages_scratch/ags/app/Main.hs
@@ -25,20 +25,37 @@ recreateGames = do
   games <- listValidGames
   mapM_ launchGame games
 
+ifAuthorized :: (Key -> ActionM ()) -> ActionM ()
+ifAuthorized action = do
+  k <- param "key"
+  valid <- liftIO $ keyIsValid k
+  case valid of
+    False -> status unauthorized401
+    True -> action k
+
 main :: IO ()
 main = do
   recreateGames
   scottyOpts opts $ do
-    get "/key/:key/map/:type/create" $ do
-      t <- param "type"
-      k <- param "key"
-      valid <- liftIO $ keyIsValid k
-      case valid of
-        False -> status unauthorized401
-        True -> do
-          game <- liftIO $ createGame t k
-          case game of
-            Just g -> do
-              liftIO $ launchGame g
-              text $ T.pack $ show $ port g
-            Nothing -> status serviceUnavailable503
+    get "/key/:key/scoreboard" $ do
+      ifAuthorized renderScoreBoard
+    get "/key/:key/map/:type/create" $
+      ifAuthorized createAction
+
+renderScoreBoard :: Key -> ActionM ()
+renderScoreBoard key = do
+  text "Alelua"
+
+createAction :: Key -> ActionM ()
+createAction key = do
+  t <- param "type"
+  valid <- liftIO $ keyIsValid key
+  case valid of
+    False -> status unauthorized401
+    True -> do
+      game <- liftIO $ createGame t key
+      case game of
+        Just g -> do
+          liftIO $ launchGame g
+          text $ T.pack $ show $ port g
+        Nothing -> status serviceUnavailable503
