8b2e3f9d77bb6aefca381719e0610cf1aaa5dd2d
Jonn Mostovoy <amarr.industrial@gmail.com>
2017-08-06 23:21:12 +0300

    Implement splurge move handling and splurge credit

--- manpages_scratch/ags/src/Punter/Actions.hs
+++ manpages_scratch/ags/src/Punter/Actions.hs
@@ -12,28 +12,47 @@ claim p g@(Game {g_players, g_free, g_moves}) river =
     claimIfFree $ isJust $ find ((==) river) g_free
   where
     claimIfFree False = pass p g
-    claimIfFree True = g { g_players = M.update claimDo p g_players
+    claimIfFree True = g { g_players = M.update (claimDo [river]) p g_players
                          , g_moves   = g_moves ++ [claimingMove p river]
                          , g_free    = filter ((/=) river) g_free }
-    claimDo pr@(Punter {claimed, splurges}) = Just $
-        pr { claimed = (river:claimed)
-           , splurges = unsplurge splurges 1 }
+
+claimDo :: [River] -> Punter -> Maybe Punter
+claimDo rs pr@(Punter {claimed, splurges}) = Just $
+    pr { claimed = claimed ++ rs
+       , splurges = unsplurge splurges $ toi $ length rs }
 
 pass :: Integer -> Game -> Game
-pass p g@(Game {g_moves}) = g { g_moves = g_moves ++ [passingMove p] }
+pass p g@(Game {g_moves, g_players}) =
+    g { g_moves = g_moves ++ [passingMove p]
+      , g_players = M.update
+                    (\u@Punter{splurges} -> Just $ u {splurges = splurges + 1})
+                    p
+                    g_players }
+
+pass' :: Integer -> Game -> Game
+pass' p g@(Game {g_moves}) = g { g_moves = g_moves ++ [passingMove p] }
 
 splurge :: Integer -> [Integer] -> Game -> Game
-splurge p route (g@Game {g_free}) =
-    splurgeIfFree $ isJust $ toRivers route g_free
+splurge p route (g@Game {g_free, g_players, g_moves}) =
+    withEnoughCredit (splurges <$> (M.lookup p g_players)) $
+    splurgeIfFree $ toRivers route g_free
   where
-    splurgeIfFree = undefined
+    splurgeIfFree Nothing = pass p g
+    splurgeIfFree (Just (toClaim, free1)) =
+        g { g_players = M.update (claimDo toClaim) p g_players
+          , g_moves   = g_moves ++ [splurgingMove p route ]
+          , g_free    = free1 }
+    withEnoughCredit (Just credit) f
+        | credit >= toi ((length route) - 1) = f
+        | otherwise = pass p g
+    withEnoughCredit Nothing _ = pass p g
 
-toRivers :: [Integer] -> [River] -> Maybe [River]
-toRivers xs rivers = toRiversDo xs rivers (Just [])
+toRivers :: [Integer] -> [River] -> Maybe ([River], [River])
+toRivers route rivers = toRiversDo route rivers (Just [])
   where
     toRiversDo _ _ Nothing = Nothing
     toRiversDo [] _ _ = Nothing
-    toRiversDo [x] _ acc = acc
+    toRiversDo [_x] free1 (Just acc) = Just (free1, acc)
     toRiversDo (x:y:xs) rivers1 (Just acc) =
         let maybeRiver =
                 oneOfTwo (find ((==) $ River x y) rivers)
@@ -67,4 +86,6 @@ unsplurge :: Integer -> Integer -> Integer
 unsplurge 0 _ = 0
 unsplurge x y
     | x >= y = x - y
-    | otherwise = error "Sploosh"
+    | otherwise = error "Sploosh"  -- Walking the thin ice
+
+toi = toInteger
