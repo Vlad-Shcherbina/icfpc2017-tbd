47a714d40580ff61a317940a3760b657d349d70e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2017-08-12 21:13:31 +0200

    Add Connection class

---
+++ server/connection.py
@@ -0,0 +1,168 @@
+import socket
+import time
+import json
+import re
+from typing import NamedTuple, Union
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Timeout(NamedTuple):
+    pass
+
+
+class Dead(NamedTuple):
+    reason: str
+
+
+class NetworkConnection:
+    """
+    Usage example:
+
+    conn, addr = server_socket.accept()
+    conn = NetworkConnection(conn)
+
+    conn.send(handshake_message)
+    msg = conn.receive(time.time() + 1)
+
+    # Don't want to discover that the peer disconnected when the game
+    # has started and we are sending setup messages already.
+    conn.recheck()
+    assert conn.alive
+
+    conn.send(...)
+    conn.receive(...)
+    ...
+    conn.kick('10 timeouts in a')
+    ...
+    conn.send(...)
+    conn.receive(...)
+    ...
+    conn.close()
+    """
+
+    def __init__(self, socket):
+        self.socket = socket
+        self.reason_dead = None
+        self.buf = bytearray()
+
+    @property
+    def alive(self):
+        return not self.reason_dead
+
+    def send(self, msg: dict):
+        """Never fails."""
+
+        if not self.alive:
+            return
+
+        s = json.dumps(msg).encode()
+        s = b'%d:%b' % (len(s), s)
+        logger.debug(f'send({s})')
+
+        self.socket.settimeout(0.5)
+        try:
+            self.socket.sendall(s)
+        except socket.timeout:
+            self.reason_dead = 'send timed out'
+        except OSError:
+            logger.exception('send failed')
+            self.reason_dead = 'send failed'
+
+    def _receive(self, deadline) -> Union[dict, Dead]:
+        buf = self.buf
+
+        while True:
+            colon_pos = buf.find(b':')
+            if colon_pos >= 0:
+                break
+            if len(buf) > 9:
+                self.reason_dead = 'message length is too long'
+                return Dead(reason=self.reason_dead)
+            self.socket.settimeout(max(1e-3, deadline - time.time()))
+            buf.extend(self.socket.recv(4096))
+
+        if colon_pos > 9:
+            self.reason_dead = 'message length is too long'
+            return Dead(reason=self.reason_dead)
+
+        if not re.match(b'\d+$', buf[:colon_pos]):
+            self.reason_dead = 'message length is not a number'
+            return Dead(reason=self.reason_dead)
+
+        msg_len = int(buf[:colon_pos].decode())
+        assert 0 <= msg_len < 1_000_000_000
+
+        msg_end = colon_pos + 1 + msg_len
+        while len(buf) < msg_end:
+            self.socket.settimeout(max(1e-3, deadline - time.time()))
+            buf.extend(self.socket.recv(4096))
+
+        try:
+            res = json.loads(buf[colon_pos + 1 : msg_end])
+        except json.JSONDecodeError:
+            self.reason_dead = 'not a valid JSON'
+            return Dead(reason=self.reason_dead)
+
+        if not isinstance(res, dict):
+            self.reason_dead = 'not a dict'
+            return Dead(reason=self.reason_dead)
+
+        del buf[:msg_end]
+        logger.debug(f'recv({res})')
+
+        return res
+
+    def receive(self, deadline) -> Union[dict, Timeout, Dead]:
+        if not self.alive:
+            return Dead(reason=self.reason_dead)
+
+        try:
+            return self._receive(deadline)
+        except socket.timeout:
+            return Timeout()
+        except OSError:
+            logger.exception('recv failed')
+            self.reason_dead = 'recv failed'
+            return Dead(reason=self.reason_dead)
+
+    def skip_received(self):
+        if not self.alive:
+            return
+        del self.buf[:]
+        self.socket.settimeout(1e-3)
+        try:
+            while True:
+                self.socket.recv(4096)
+        except socket.timeout:
+            pass
+        except OSError:
+            logger.exception('recv failed')
+            self.reason_dead = 'recv failed'
+
+    def recheck(self):
+        if not self.alive:
+            return
+        self.socket.settimeout(1e-3)
+        try:
+            data = self.socket.recv(1)
+        except socket.timeout:
+            return
+        except OSError:
+            logger.exception('recheck failed')
+            self.reason_dead = 'recheck failed'
+        if not data:
+            self.reason_dead = 'peer disconnected'
+        self.buf.extend(data)
+
+    def kick(self, reason):
+        if not self.alive:
+            return
+
+        self.reason_dead = f'kicked ({reason})'
+        self.socket.close()
+        self.socket = None
+
+    def close(self):
+        if self.socket is not None:
+            self.socket.close()
---
+++ server/connection_test.py
@@ -0,0 +1,164 @@
+import socket
+import time
+import threading
+
+import pytest
+
+from connection import NetworkConnection, Timeout, Dead
+
+
+@pytest.fixture
+def connect():
+    address = '127.0.0.1', 42424
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(address)
+    server.listen(1)
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(address)
+
+    conn, addr = server.accept()
+    conn = NetworkConnection(conn)
+
+    yield conn, client
+
+    conn.close()
+    server.close()
+
+
+def test_simple(connect):
+    conn, client = connect
+
+    client.sendall(b'8:{"hi":1}')
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == dict(hi=1)
+
+    conn.send(dict(q=2))
+    data = client.recv(4096)
+    assert data == b'8:{"q": 2}'
+
+    assert conn.alive
+
+
+def test_timeout(connect):
+    conn, client = connect
+
+    msg = conn.receive(deadline=time.time() + 0.5)
+    assert msg == Timeout()
+
+
+def test_skip_receive(connect):
+    conn, client = connect
+
+    msg = conn.receive(deadline=time.time() + 0.5)
+    assert msg == Timeout()
+    client.send(b'stuff')
+
+    conn.skip_received()
+
+    conn.send({})
+    data = client.recv(4096)
+    assert data == b'2:{}'
+    client.send(b'2:{}')
+
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == {}
+
+
+def test_recheck(connect):
+    conn, client = connect
+
+    conn.recheck()
+    assert conn.alive
+
+    client.send(b'2:{}')
+    conn.recheck()
+    assert conn.alive
+
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == {}
+
+    client.close()
+    conn.recheck()
+    assert conn.reason_dead == 'peer disconnected'
+
+    conn.recheck()
+    assert conn.reason_dead == 'peer disconnected'
+
+
+def test_kick(connect):
+    conn, client = connect
+
+    client.settimeout(1e-3)
+    try:
+        client.recv(4096)
+        assert False
+    except socket.timeout:
+        pass
+
+    conn.kick('test')
+
+    data = client.recv(4096)
+    assert data == b''
+
+    conn.send({'hi': 1})
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == Dead(reason='kicked (test)')
+    assert not conn.alive
+
+    conn.kick('again')
+
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == Dead(reason='kicked (test)')
+    assert not conn.alive
+
+
+def test_length_no_colon(connect):
+    conn, client = connect
+
+    client.send(b'1234567890')
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == Dead(reason='message length is too long')
+    assert not conn.alive
+
+
+def test_length_too_long(connect):
+    conn, client = connect
+
+    client.send(b'1234567890:{}')
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == Dead(reason='message length is too long')
+    assert not conn.alive
+
+
+def test_length_not_number(connect):
+    conn, client = connect
+
+    client.send(b'zzz:{}')
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == Dead(reason='message length is not a number')
+    assert not conn.alive
+
+
+def test_length_not_valid_json(connect):
+    conn, client = connect
+
+    client.send(b'3:zzz')
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == Dead(reason='not a valid JSON')
+    assert not conn.alive
+
+
+def test_length_not_dict(connect):
+    conn, client = connect
+
+    client.send(b'1:1')
+    msg = conn.receive(deadline=time.time() + 1)
+    assert msg == Dead(reason='not a dict')
+    assert not conn.alive
+
+
+if __name__ == '__main__':
+    import sys, logging, pytest
+    logging.basicConfig(level=logging.DEBUG)
+    pytest.main([__file__] + sys.argv[1:])
